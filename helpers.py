1a1888960a7d91a64d5a21226c990ec73eff12b8
ilikehaskell <ouatubogdanioan@gmail.com>
2021-12-08 06:06:18 +0200

    comment play sound

--- helpers.py
+++ helpers.py
@@ -10,5 +10,6 @@ def tensor_to_image(tensor):
         tensor = tensor[0]
     return PIL.Image.fromarray(tensor)
 
-def play_sound():
-    return  """beep = np.sin(2*np.pi*400*np.arange(100000*2)/10000),ipd.Audio(beep, rate=100000, autoplay=True)"""
+# def play_sound():
+#     beep = np.sin(2*np.pi*400*np.arange(100000*2)/10000)
+#     ipd.Audio(beep, rate=100000, autoplay=True)
