12c97efcd9aa6a204a8757b1f728bbf282896fbf
ilikehaskell <ouatubogdanioan@gmail.com>
2021-12-12 22:58:00 +0200

    correct plots

--- plots.py
+++ plots.py
@@ -5,6 +5,7 @@ def get_images_with_labels(ilp):
     labels = [str(int(label)+1) for label in labels]
     preds = [str(int(pred)+1) for pred in preds]
     plot_labels = [''.join([il, ip]) for il, ip in zip(labels, preds)]
-    imgs = [tensor_to_image(img.permute(1,2,0).cpu()) for img in imgs]
+    imgs = [tensor_to_image(img.permute(1,2,0).squeeze().cpu()) for img in imgs]
+    
     return imgs, plot_labels
 
