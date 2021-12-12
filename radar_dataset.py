35034645530f7110b8aa78450355cfaec9dca164
ilikehaskell <ouatubogdanioan@gmail.com>
2021-12-12 04:05:56 +0200

    siammese exp

--- radar_dataset.py
+++ radar_dataset.py
@@ -7,7 +7,10 @@ from PIL import Image
 
 from torch.utils.data import Dataset
 
-
+import random
+import torch
+import numpy as np
+from collections import defaultdict
 
 class RadarDataset(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
@@ -15,7 +18,7 @@ class RadarDataset(Dataset):
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
-
+        
     def __len__(self):
         return len(self.img_labels)
     
@@ -33,3 +36,46 @@ class RadarDataset(Dataset):
         if self.target_transform:
             label = self.target_transform(label)
         return image, label - 1
+
+class SiammeseDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+        self.num_classes = 5
+        self.datas = {}
+        for i in range(5):
+            self.datas[i] = [ img for img in  self.img_labels[self.img_labels.label == 1].id]
+
+    def __len__(self):
+        return 21000000
+    
+    def __getitem__(self, index):
+        # img_path = os.path.join(self.img_dir, self.img_labels.iloc[index, 0])
+        # image = Image.open(img_path)
+        
+         # image1 = random.choice(self.dataset.imgs)
+        label = None
+        img1 = None
+        img2 = None
+        # get image from same class
+        if index % 2 == 1:
+            label = 1.0
+            idx1 = random.randint(0, self.num_classes - 1)
+            image1 = random.choice(self.datas[idx1])
+            image2 = random.choice(self.datas[idx1])
+        # get image from different class
+        else:
+            label = 0.0
+            idx1 = random.randint(0, self.num_classes - 1)
+            idx2 = random.randint(0, self.num_classes - 1)
+            while idx1 == idx2:
+                idx2 = random.randint(0, self.num_classes - 1)
+            image1 = random.choice(self.datas[idx1])
+            image2 = random.choice(self.datas[idx2])
+        image1, image2 = Image.open(os.path.join(self.img_dir,image1)), Image.open(os.path.join(self.img_dir,image2))
+        if self.transform:
+            image1 = self.transform(image1)
+            image2 = self.transform(image2)
+        return image1, image2, torch.from_numpy(np.array([label], dtype=np.float32))
