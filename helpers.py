e64d82e1f2dbd5ae08333ab76c11c7df763aa0f6
ilikehaskell <ouatubogdanioan@gmail.com>
2021-12-12 16:32:44 +0200

    save as submission helper

--- helpers.py
+++ helpers.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np, pandas as pd
 import PIL
 import IPython.display as ipd
 
@@ -9,6 +9,14 @@ def tensor_to_image(tensor):
         assert tensor.shape[0] == 1
         tensor = tensor[0]
     return PIL.Image.fromarray(tensor)
+
+def save_as_submission_csv(model, test_holder,test_csv, save_path):
+    results = test_holder.get_input_label_preds(model)
+    preds = [( c+1) for a,b,c in results]
+    test_df = pd.read_csv(test_csv) 
+    test_df['label'] = preds
+    test_df.to_csv(save_path, index=False)
+    return test_df
 
 # def play_sound():
 #     beep = np.sin(2*np.pi*400*np.arange(100000*2)/10000)
